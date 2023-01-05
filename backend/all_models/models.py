8e7a9dc8c5ff9d6bdbd0f5940d286da17ee42d0c
Nikita <Nif-Naf94@yandex.ru>
2023-01-05 14:39:56 +0300

    Добавил обязательные аргументы.
    Добавил обязательные именованные аргументы полям CharField у моделей.

--- backend/all_models/models.py
+++ backend/all_models/models.py
@@ -6,14 +6,14 @@ from django.db.models import (
 
 class Tenant(Model):
     """Модель описывающая сущность: Жильца."""
-    tenant_all_name = CharField()
+    tenant_all_name = CharField(max_length=50)
 
 
 class Worker(Model):
     """Модель описывающая сущность: Сотрудника."""
-    full_name = CharField()
+    full_name = CharField(max_length=50)
 
 
 class Provider(Model):
     """Модель описывающая сущность: Организации."""
-    name_organization = CharField()
+    name_organization = CharField(max_length=50)
