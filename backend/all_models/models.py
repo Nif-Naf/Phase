fc6d7c6846bb66a0947561309e47edaaa4a4ea09
Nikita <Nif-Naf94@yandex.ru>
2023-01-05 14:27:09 +0300

    Создание простых моделей.
    В данный момент у каждой модели по одному полю. В дальнейшем расширим.

--- backend/all_models/models.py
+++ backend/all_models/models.py
@@ -1,3 +1,19 @@
-from django.db import models
+from django.db.models import (
+    Model,
+    CharField
+)
 
-# Create your models here.
+
+class Tenant(Model):
+    """Модель описывающая сущность: Жильца."""
+    tenant_all_name = CharField()
+
+
+class Worker(Model):
+    """Модель описывающая сущность: Сотрудника."""
+    full_name = CharField()
+
+
+class Provider(Model):
+    """Модель описывающая сущность: Организации."""
+    name_organization = CharField()
